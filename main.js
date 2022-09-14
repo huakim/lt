dda9cbd191823f1830a74207d68dbaa2a3adcf69
huakim <92579808+huakim@users.noreply.github.com>
2022-09-14 14:40:03 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-//<link rel=stylesheet href=main.css />
-var hd = document.getElementsByTagName("head")[0];
-var ln = document.createElement('link');
-var host = "https://huakim.github.io/lt/";
-ln.href = host + 'main.css';
-ln.rel='stylesheet';
-hd.appendChild(ln);
-hd = document.getElementsByTagName("body")[0];
-ln = document.createElement('iframe');
-ln.src = host + 'main.html';
-hd.appendChild(ln);
-delete hd;
-delete host;
-var dirName;
-var tt = this;
-ln.addEventListener("load", function() {
-    ln.contentWindow.main.call(tt, START_PAGE);
-});
+var rc = new XMLHttpRequest();
+rc.open("GET", "https://huakim.github.io/lt/main.html", false);
+rc.send();
+
+document.querySelector('html').innerHTML = rc.response;
