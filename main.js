757cb35b209839ebc2536e9644b289825fdfdd4b
huakim <92579808+huakim@users.noreply.github.com>
2022-09-14 14:28:47 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //<link rel=stylesheet href=main.css />
 var hd = document.getElementsByTagName("head")[0];
 var ln = document.createElement('link');
+var host = "https://huakim.github.io/lt/";
 ln.href = host + 'main.css';
 ln.rel='stylesheet';
 hd.appendChild(ln);
@@ -9,6 +10,7 @@ ln = document.createElement('iframe');
 ln.src = host + 'main.html';
 hd.appendChild(ln);
 delete hd;
+delete host;
 var dirName;
 var tt = this;
 ln.addEventListener("load", function() {
